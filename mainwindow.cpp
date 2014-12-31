39bef3b7eaaa62c5061166e06333d4c372cbe6ad
owata-programer <owata-programer@outlook.com>
2014-12-31 22:28:44 +0900

    ログを出すようにした

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -51,14 +51,85 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->aboutQtAction, &QAction::triggered, [&]() {
         QMessageBox::aboutQt(this, tr("Qtについて"));
     });
+    //ログ
+    connect(&m_updateName, &UpdateName::started, [&]() {
+        QMetaObject::invokeMethod(ui->updateNameSwitch, "setChecked", Qt::QueuedConnection, Q_ARG(bool, true));
+        QMetaObject::invokeMethod(ui->logText, "appendPlainText", Qt::QueuedConnection, Q_ARG(QString, tr("update_nameを起動しました。")));
+    });
+    connect(&m_updateName, &UpdateName::screenNameLookuped, [&](const QString &screenName) {
+        QMetaObject::invokeMethod(ui->logText, "appendPlainText", Qt::QueuedConnection,
+                                  Q_ARG(QString, tr("screen_nameを取得しました。\nあなたのscreen_nameは@%1です。").arg(screenName)));
+    });
+    connect(&m_updateName, &UpdateName::running, [&]() {
+        QMetaObject::invokeMethod(ui->logText, "appendPlainText", Qt::QueuedConnection, Q_ARG(QString, tr("update_nameを開始しました。")));
+    });
+    connect(&m_updateName, &UpdateName::stopping, [&]() {
+        QMetaObject::invokeMethod(ui->logText, "appendPlainText", Qt::QueuedConnection, Q_ARG(QString, tr("update_nameを終了しています。")));
+    });
+    connect(&m_updateName, &UpdateName::finished, [&]() {
+        QMetaObject::invokeMethod(ui->updateNameSwitch, "setChecked", Qt::QueuedConnection, Q_ARG(bool, false));
+        QMetaObject::invokeMethod(ui->logText, "appendPlainText", Qt::QueuedConnection, Q_ARG(QString, tr("update_nameを終了しました。")));
+    });
+    connect(&m_updateName, &UpdateName::error, [&](UpdateName::State state, const QString &errorMessage) {
+        QMetaObject::invokeMethod(ui->logText, "appendPlainText", Qt::QueuedConnection, Q_ARG(QString, tr("%1のエラー: %2").arg(state == UpdateName::UserStream
+                                                                                                                            ? "ユーザーストリーム" : "スクリーンネーム取得",
+                                                                                                                            errorMessage)));
+    });
+    connect(&m_updateName, &UpdateName::updateStarted, [&](UpdateProfile::UpdateType type, const TwitterAPI::Object::Users &executedUser) {
+        QMetaObject::invokeMethod(ui->logText, "appendPlainText", Qt::QueuedConnection, Q_ARG(QString, tr("\"%1@%2\"がupdate_%3を実行しました")
+                                                                                              .arg(executedUser.name(), executedUser.screenName(), updateTypeToString(type))));
+    });
+    connect(&m_updateName, &UpdateName::updateFinished, [&](UpdateProfile::UpdateType type, const QString &newProfile) {
+        QMetaObject::invokeMethod(ui->logText, "appendPlainText", Qt::QueuedConnection, Q_ARG(QString, tr("%1を\"%2\"に変更しました。").arg(updateTypeToString(type), newProfile)));
+    });
+    connect(&m_updateName, &UpdateName::resultPosted, [&]() {
+        QMetaObject::invokeMethod(ui->logText, "appendPlainText", Qt::QueuedConnection, Q_ARG(QString, tr("結果をツイートしました。")));
+    });
+    connect(&m_updateName, &UpdateName::updateError, [&](UpdateProfile::UpdateType type, const QString &errorMessage) {
+        QMetaObject::invokeMethod(ui->logText, "appendPlainText", Qt::QueuedConnection, Q_ARG(QString, tr("update_%1に失敗しました。: %2").arg(updateTypeToString(type), errorMessage)));
+    });
+    connect(&m_updateName, &UpdateName::resultPostError, [&](const QString &errorMessage) {
+        QMetaObject::invokeMethod(ui->logText, "appendPlainText", Qt::QueuedConnection, Q_ARG(QString, tr("結果のツイートに失敗しました。: %1").arg(errorMessage)));
+    });
 
-    connect(ui->updateNameSwitch, SIGNAL(clicked()), &m_updateName, SLOT(start()));
+    connect(ui->updateNameSwitch, &QPushButton::clicked, [&]() {
+        if (ui->updateNameSwitch->isChecked()) {
+            ui->updateNameSwitch->setChecked(false);
+            m_updateName.start();
+        } else {
+            ui->updateNameSwitch->setChecked(true);
+            m_updateName.stop();
+        }
+    });
+    connect(ui->updateNameSwitch, &QPushButton::toggled, [&](bool checked) {
+            ui->updateNameSwitch->setText(tr(checked ? "update_nameを終了する" : "update_nameを開始する"));
+    });
 }
 
 MainWindow::~MainWindow()
 {
-    //m_updateName.quit();
     m_updateName.stop();
     m_updateName.wait();
     delete ui;
+}
+
+QString MainWindow::updateTypeToString(UpdateProfile::UpdateType type)
+{
+    switch (type) {
+    case UpdateProfile::Name:
+        return "name";
+    case UpdateProfile::Url:
+        return "url";
+    case UpdateProfile::Location:
+        return "location";
+    case UpdateProfile::Description:
+        return "description";
+    case UpdateProfile::Image:
+        return "image";
+    case UpdateProfile::Background:
+        return "background";
+    case UpdateProfile::Banner:
+        return "banner";
+    }
+    return NULL;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -19,6 +19,8 @@ public:
     ~MainWindow();
 
 private:
+    QString updateTypeToString(UpdateProfile::UpdateType type);
+
     Ui::MainWindow *ui;
     PreferencesDialog m_preferencesDialog;
     UpdateName m_updateName;
